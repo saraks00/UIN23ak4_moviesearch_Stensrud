48b3401cf85b2919e463dda754cee677e4cc515d
saraks00 <sarakristine@hotmail.no>
2023-03-18 21:07:09 +0100

    Satte opp React, Sass og begynte med layout

---
+++ moviesearch/src/App.js
@@ -0,0 +1,17 @@
+import './css/main.css'
+
+function App() {
+  return (
+  <div id="layoutCont">
+    <header>
+      <h1>Filmgalleri</h1>
+    </header>
+    <main>
+      <input type="text"></input>
+      <button>Søk</button>
+    </main>
+  </div>
+  )
+}
+
+export default App;
